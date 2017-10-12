71b4c7f5e5acd10377e5f227a6164880c32c612a
Ankit Goila <agoila@umich.edu>
2017-10-11 23:20:42 -0400

    initial commit

---
+++ Week 1/Assignments/max_pairwise_product.py
@@ -0,0 +1,18 @@
+# Uses python3
+n = int(input())
+a = [int(x) for x in input().split()]
+assert(len(a) == n)
+
+max_index1 = -1
+
+for i in range(0, n):
+    if max_index1 == -1 or a[i] > a[max_index1]:
+        max_index1 = i
+
+max_index2 = -1
+
+for j in range(0, n):
+    if (a[j] != a[max_index1]) and (max_index2 == -1 or a[j] > a[max_index2]):
+        max_index2 = j
+
+print(a[max_index1] * a[max_index2])
